15d1ad8ff12c1d797380d2fedb57cddf5c893557
prasannabalajims <prasannabalajims@gmail.com>
2023-10-26 23:45:09 +0530

    Initial commit

---
+++ src/main/java/self/learn/containerization/SpringContainerizationApplication.java
@@ -0,0 +1,13 @@
+package self.learn.containerization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringContainerizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringContainerizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/self/learn/containerization/SpringContainerizationApplicationTests.java
@@ -0,0 +1,13 @@
+package self.learn.containerization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringContainerizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
